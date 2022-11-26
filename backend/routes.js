fc0b32d0a33e840382686f23dc82bb533e4deb1c
Giorgio <tran.giorgio@gmail.com>
2022-11-25 23:22:06 -1000

    Add .rest and router middleware

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const controllers=require('./controllers');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log('router is working');
+  next();
+});
+
 router.get('/mail/user/:email',controllers.getUser)
 router.post('/mail/send',controllers.sendMail);
 router.get('/mail/drafts/:email', controllers.getDrafts);
